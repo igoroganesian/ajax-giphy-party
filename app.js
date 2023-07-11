70cf18d488eca48dea44b8a76395c348b1c782b3
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-11 19:49:11 -0400

    axios.get request working

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("Let's get this party started!");
+"use strict";
+
+const GIPHY_URL = "http://api.giphy.com/v1/gifs/search";
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+"http://api.giphy.com/v1/gifs/search"
+"?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+
+const $input = $("#input");
+const $gifContainer = $("#container");
+
+$("#form")
+const $submitButton = $("#submit-button");
+
+
+
+
+
+/** get results from giphy */
+
+async function getGiphyImages() {
+const searchTerm = "cats"; //change to function results
+
+  let giphyResults = await axios.get(
+    GIPHY_URL, { params: {q: searchTerm, api_key: API_KEY}}
+    );
+
+  return giphyResults;
+}
+
+
+
+
+
+/** on submit, get info and add to ??? */
+
+function getSubmitInfo() {
+
+  $submitButton.on("click", )
+}
+
+/** add image to site */
+
