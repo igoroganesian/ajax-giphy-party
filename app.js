ca78830d8d2f1a585f1e5284f5bfe934d44e6f1f
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-11 20:18:22 -0400

    changed functions, resolving git issue

--- app.js
+++ app.js
@@ -19,14 +19,24 @@ const $submitButton = $("#submit-button");
 /** get results from giphy */
 
 async function getGiphyImages() {
-const searchTerm = "cats"; //change to function results
+const searchTerm = $input.val(); //change to function results
 
   let giphyResults = await axios.get(
     GIPHY_URL, { params: {q: searchTerm, api_key: API_KEY}}
     );
 
   return giphyResults;
+  //returns object with data property containing ~50 gifs; make
+  //function to access random gif (0-49)
 }
+
+function pickRandomGifIndex() {
+  const randomIndex = (Math.floor(Math.random() * 50));
+  return randomIndex;
+  //eventually giphyResults.data[randomIndex];
+}
+
+
 
 
 
@@ -34,10 +44,16 @@ const searchTerm = "cats"; //change to function results
 
 /** on submit, get info and add to ??? */
 
-function getSubmitInfo() {
+async function getSubmitInfo(evt) {
+  evt.preventDefault();
 
-  $submitButton.on("click", )
+  const urls = getGiphyImages();
+
+  addImage(urls);
 }
 
 /** add image to site */
 
+// function addImage() {
+
+// }
